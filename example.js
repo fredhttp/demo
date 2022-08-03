c1fc7b578242ce7f1c70fd858567753cda440348
fredhttp <fredhttp@gmail.com>
2022-08-03 12:32:24 -0700

    first

---
+++ example.js
@@ -0,0 +1,29 @@
+/*
+ * 
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+
+xyx sssssssssssssssssssssssssssssssssssssssssss
+ *
+
+ *
+ 
+*/
+
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+   
+context.font = '38pt Arial';
+context.fillStyle = 'cornflowerblue';
+context.strokeStyle = 'blue';
+
+context.fillText("Hello Canvas", canvas.width/2 - 150,
+                                 canvas.height/2 + 15);
+
+context.strokeText("Hello Canvas", canvas.width/2 - 150,
+                                   canvas.height/2 + 15 );
